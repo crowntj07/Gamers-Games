25d04339b65ee0cc1951d2f49f7e78ad7041db62
Todd Jones <jonesgtodd@gmail.com>
2020-12-05 18:09:29 -0600

    added code to app.js

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require('express')
 
 const app = express();
 
+//Middleware
+app.use('/posts', () => {
+    console.log('Middleware is running');
+
+
+});
+
+
+
+
 //Routes
 app.get('/', (req,res) => {
     res.send('We are LIVE');
 
 });
+
+app.get('/posts', (req,res) => {
+    res.send('We are LIVE');
+
+});
+
 
 
 
