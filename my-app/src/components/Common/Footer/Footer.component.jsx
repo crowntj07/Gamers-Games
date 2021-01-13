84e55d8d50eb21e661c66ad14ed63f7a08b09b18
Todd Jones <jonesgtodd@gmail.com>
2021-01-13 16:09:11 -0600

    create a Games folder

---
+++ my-app/src/components/Common/Footer/Footer.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.styles.scss'
+
+const Footer = () => (
+  <footer className="text-center"> {new Date().getFullYear()} - &copy; Todd Jones</footer>
+)
+
+export default Footer;
