20367fdda7c2fe22b84c64a84adcbe634c016ce4
Todd Jones <jonesgtodd@gmail.com>
2021-01-13 20:16:23 -0600

    working on login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import TopGames from "./TopGames";
-import Search from "./Search";
-import Nav from "./Nav";
-import GameDetail from "./GameDetail";
-
-function App() {
-  
-  return (
-    <Router>
+import Loginscreen from './Loginscreen'
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
+  render() {
+    return (
       <div className="App">
-        <Nav />
-        <Switch>
-          <Route exact path='/topgames' component={TopGames} />
-          <Route exact path='/search' component={Search} />
-          <Route path='/game/:name' component={GameDetail} />
-        </Switch>
+        {this.state.loginPage}
+        {this.state.uploadScreen}
       </div>
-    </Router>
-  );
+    );
+  }
 }
-
+const style = {
+  margin: 15,
+};
 export default App;
